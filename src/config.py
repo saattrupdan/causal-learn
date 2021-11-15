003919f55ecf7dce80f00cfebe8b6b53009a28c7
saattrupdan <saattrupdan@gmail.com>
2021-11-15 22:06:04 +0000

    feat: Update hyperparameters

--- src/config.py
+++ src/config.py
@@ -5,12 +5,14 @@ class Config:
     '''Config class used to store all hyperparameters'''
 
     # Set default hyperparameters
-    dim: int = 16
+    dim: int = 100
     num_iterations: int = 1_000_000
-    dropout: float = 0.5
+    dropout: float = 0.0
     lr: float = 3e-4
     ema_decay: float = 0.9
-    batch_size: int = 100
+    batch_size: int = 32
+    model_path: str = './models/model.pt'
+    threshold: float = 0.5
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
