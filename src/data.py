79ca477fa944a83059e2771cc165d3779117d30f
saattrupdan <saattrupdan@gmail.com>
2021-11-15 19:57:09 +0000

    feat: Change dataloader.py to data.py

--- src/data.py
+++ src/data.py
@@ -1,9 +1,11 @@
 '''PyTorch dataloader to load correlation matrices and their CPDAGs'''
 
-from torch.utils.data import IterableDataset, DataLoader
+from torch.utils.data import IterableDataset
+from torch_geometric.data import HeteroData
+from torch_geometric.utils.convert import from_scipy_sparse_matrix
 import numpy as np
 import torch
-from typing import Optional, Tuple, Iterable
+from typing import Optional, Tuple
 from dag_sampler import DAGSampler
 from gaussian_sampler import GaussianDataSampler
 
@@ -51,8 +53,28 @@ class CorrDataset(IterableDataset):
             # Compute the correlation matrix of `data_matrix`
             corr_matrix = np.corrcoef(data_matrix.T)
 
+            # Store the number of features
+            num_feats = data_matrix.shape[1]
+
+            # Get the edge_index of the CPDAG
+            cpdag_edge_idx, _ = from_scipy_sparse_matrix(cpdag)
+
+            # Organise the input as a PyG graph, to be inputted to the model
+            graph_data = HeteroData()
+            graph_data['feat'].x = torch.ones((num_feats, 1))
+            graph_data['feat', 'correlated_with', 'feat'].edge_index = \
+                torch.ones((2, num_feats * num_feats)).long()
+            graph_data['feat', 'correlated_with', 'feat'].edge_attr = \
+                (torch.tensor(corr_matrix)
+                      .view(num_feats * num_feats, 1)
+                      .float())
+            graph_data['feat', 'implies', 'feat'].edge_index = cpdag_edge_idx
+
+            # Convert the cpdag adjacency matrix to a PyTorch tensor
+            cpdag = torch.from_numpy(cpdag.todense()).float()
+
             # Return the data
-            yield torch.from_numpy(corr_matrix), torch.from_numpy(cpdag)
+            yield graph_data, cpdag
 
     def __iter__(self):
         '''Iterate over the dataset'''
@@ -62,13 +84,14 @@ class CorrDataset(IterableDataset):
 if __name__ == '__main__':
     from tqdm.auto import tqdm
     from itertools import islice
+    from torch_geometric.loader import DataLoader
+
     dataset = CorrDataset()
     dataloader = DataLoader(dataset)
 
     for batch in dataloader:
-        corr_matrix, cpdag = batch
-        print(corr_matrix)
-        print(cpdag)
+        data = batch
+        print(data)
         break
 
     with tqdm(total=100) as pbar:
