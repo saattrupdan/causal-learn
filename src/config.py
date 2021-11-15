d864c0e3295e055690219a2f0d7965946f2099a3
saattrupdan <saattrupdan@gmail.com>
2021-11-15 22:12:34 +0000

    docs: Organise the hyperparameters

--- src/config.py
+++ src/config.py
@@ -6,17 +6,25 @@ from typing import Tuple
 class Config:
     '''Config class used to store all hyperparameters'''
 
-    # Set default hyperparameters
-    dim: int = 100
-    num_iterations: int = 1_000_000
-    dropout: float = 0.0
-    lr: float = 3e-4
-    ema_decay: float = 0.999
-    batch_size: int = 32
-    model_path: str = './models/model.pt'
-    threshold: float = 0.5
+    # Dataset hyperparameters
     num_variables_range: Tuple[int, int] = (3, 10)
     num_data_points_range: Tuple[int, int] = (100, 10_000)
+
+    # Model hyperparameters
+    dim: int = 100
+    dropout: float = 0.0
+
+    # Training hyperparameters
+    num_iterations: int = 1_000_000
+    lr: float = 3e-4
+    batch_size: int = 32
+
+    # Metric hyperparameters
+    threshold: float = 0.5
+    ema_decay: float = 0.999
+
+    # Miscellaneous hyperparameters
+    model_path: str = './models/model.pt'
     random_seed: int = 4242
 
     def __init__(self, **kwargs):
