e12d023fa06ca615d13c223d44e6b24de249e1ee
saattrupdan <saattrupdan@gmail.com>
2021-11-16 15:30:51 +0000

    fix: Update config from class variables

--- src/config.py
+++ src/config.py
@@ -1,34 +1,42 @@
 '''Config class used to store all hyperparameters'''
 
 from typing import Tuple
+import datetime as dt
 
 
 class Config:
     '''Config class used to store all hyperparameters'''
 
     # Dataset hyperparameters
-    num_variables_range: Tuple[int, int] = (3, 10)
-    num_data_points_range: Tuple[int, int] = (100, 10_000)
-    sampling_random_seed: int = 4242
+    num_variables_range: Tuple[int, int] = (2, 10)
+    num_data_points : int = 10_000
 
     # Model hyperparameters
-    dim: int = 100
+    dim: int = 250
     dropout: float = 0.0
 
     # Training hyperparameters
     num_iterations: int = 1_000_000
     lr: float = 3e-4
-    batch_size: int = 32
+    batch_size: int = 1
+    threshold: float = 0.5
 
     # Metric hyperparameters
-    threshold: float = 0.5
-    ema_decay: float = 0.999
+    ema_decay: float = 0.99
 
     # Miscellaneous hyperparameters
-    model_path: str = './models/model.pt'
+    _datetime = dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    model_dir: str = f'./models/{_datetime}-model'
 
     def __init__(self, **kwargs):
+        # Update the config with the values of the class variables
+        class_vars = {key: getattr(self, key) for key in dir(self)
+                      if not key.startswith('__')}
+        self.__dict__.update(class_vars)
+
+        # Update the config with the values of the kwargs
         self.__dict__.update(kwargs)
+
 
     def __repr__(self):
         return str(self.__dict__)
